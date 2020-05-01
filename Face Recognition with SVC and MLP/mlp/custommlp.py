751b4f6884cac0fe86c17df2b2345f46a0aa8a15
T <tjbergstrom@alaska.edu>
2020-05-01 09:40:12 -0800

    Support Vector Machine

--- Face Recognition with SVC and MLP/mlp/custommlp.py
+++ Face Recognition with SVC and MLP/mlp/custommlp.py
@@ -1,4 +1,32 @@
 
+
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.optimizers import SGD
+from keras.layers import Dense
+
 class CustomMLP:
-	def __init__(self, model, processor):
-		self.model = model
+	def preprocess(dataset):
+		# not necessary
+        return dataset
+
+	def build(width, height, depth, classes):
+		input_shape = (height, width, depth)
+		model = Sequential()
+		model.add(Dense(512, input_shape=input_shape))
+		model.add(Activation("relu"))
+		model.add(Dense(128, activation="relu"))
+		model.add(Dense(classes))
+		model.add(activation=("softmax"))
+		return model
+
+	def optimize():
+		opt = SGD(lr=0.1, decay=0.00001, nesterov=True)
+		return opt
+
+	def compile(model, opt):
+		model.compile(loss='spare_categorical_crossentropy',
+		optimizer=opt, metrics='accuracy')
+		return model
+
+
--- Face Recognition with SVC and MLP/svc/customsvc.py
+++ Face Recognition with SVC and MLP/svc/customsvc.py
@@ -1,6 +1,26 @@
 
+from sklearn.svm import SVC
+import numpy as np
 
 class CustomSVC:
-	def __init__(self, model, processor):
-		self.model = model
+	def gaussian_matrix(x1, x2, sigma=0.1):
+		gram_matrix = np.zeros((x1.shape[0], x2.shape[0]))
+		for (i, x1) in enumerate(x1):
+			for (j, x2) in enumerate(x2):
+				x1 = x1.flatten()
+				x2 = x2.flatten()
+				gram_matriix[i,j] = np.exp(-np.sum(x2), 2))
+				/ float( 2*(sigma**2) ) )
+	return gram_matrix
+
+	def build(x, y):
+		model = SVC(C=0.1, kernel="precomputed")
+		model.fit(self.gaussian_matrix(x, x), y)
+		return model
+
+	def predict(trainX, testX):
+		model.predict(self.gaussian_matrix(testX, trainX)
+		return model
+
+
 
