99575eae34d229c291e5dff790a54980076c9bad
tjbergstrom <37911183+tjbergstrom@users.noreply.github.com>
2020-06-10 23:04:18 -0800

    VGG neural network model implementation

--- Neural Networks for Image Classification/vgg/vggnet.py
+++ Neural Networks for Image Classification/vgg/vggnet.py
@@ -32,7 +32,7 @@ class VGGmodel:
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(3, 3)))
-		model.add(Dropout(0.25))
+		model.add(Dropout(0.2))
 
 		# second convolutional layer
 		model.add(Conv2D(64, (3, 3), padding="same"))
@@ -42,7 +42,7 @@ class VGGmodel:
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
+		model.add(Dropout(0.2))
 
 		# third convolutional layer
 		model.add(Conv2D(128, (3, 3), padding="same"))
@@ -52,19 +52,18 @@ class VGGmodel:
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
+		model.add(Dropout(0.2))
 
 		# fully connected layer
 		model.add(Flatten())
 		model.add(Dense(1024))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization())
-		model.add(Dropout(0.5))
+		model.add(Dropout(0.2))
 
 		model.add(Dense(classes))
 		model.add(Activation("softmax"))
 
 		return model
-
 
 
