6c2f32a4322ea387e456c9e1e22be1cbc3be32d4
T <tjbergstrom@alaska.edu>
2020-08-15 05:18:22 -0800

    processing

--- Sentiment Analysis/train.py
+++ Sentiment Analysis/train.py
@@ -7,3 +7,18 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import nltk
+import os
+from nltk.corpus import stopwords
+import string
+
+
+# Pre-Processing
+
+# Remove "@user"
+# Comment out because only needs to be done once
+#os.chdir("processed_data")
+#os.system("sed -i 's/'@user'/''/g' train.csv")
+
+
+
+
