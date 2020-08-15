742b4f1f0bbd49bccd28a8959257c44d86af3222
T <tjbergstrom@alaska.edu>
2020-08-15 04:42:14 -0800

    data collection

---
+++ Sentiment Analysis/train.py
@@ -0,0 +1,9 @@
+# train.py
+# August 2020
+# Just rying out a sentiment analysis exercise
+
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import nltk
